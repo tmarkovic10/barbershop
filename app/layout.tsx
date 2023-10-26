c186a0a7a8e800d34f78744a6305214d64008ecd
ToniMarkovic <markovictoni10@gmail.com>
2023-10-26 11:02:24 +0200

    added fonts and auth styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 
-export const metadata = {
-  title: "Next.js 13 with Clerk",
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: `Barber's`,
+  description: `"Elevate your grooming experience with Barber's - your go-to destination for precision cuts, grooming, and relaxation. Explore our app for convenient booking, expert services, and a modern barbershop journey like no other."`,
+  icons: {
+    icon: "",
+  },
 };
 
 export default function RootLayout({
@@ -11,9 +32,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
