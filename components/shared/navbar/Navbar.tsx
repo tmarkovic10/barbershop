c75268c6dbff065e4caefd4ea6640f09eb6fff28
ToniMarkovic <markovictoni10@gmail.com>
2023-10-26 14:22:50 +0200

    added navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,7 +1,42 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 flex w-full p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex-center gap-3">
+        <Image
+          src="/assets/icons/barbers-favicon.svg"
+          width={50}
+          height={50}
+          alt="Barber's"
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900">
+          Barber<span className="text-primary-500">&apos;s</span>
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        <Theme />
+
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
